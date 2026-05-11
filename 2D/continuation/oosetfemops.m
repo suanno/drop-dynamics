95a60bf9466530f4d56ddb0b03339d859263bd4f
suanno <xroccosuannox@gmail.com>
2026-05-11 15:43:53 +0200

    .

--- 2D/continuation/oosetfemops.m
+++ 2D/continuation/oosetfemops.m
@@ -1,5 +1,5 @@
 function p=oosetfemops(p) % for 1D Neuman bc
 fem=p.pdeo.fem; gr=p.pdeo.grid; 
 x=getpte(p); x=x';
-[p.mat.K,p.mat.M,~]=fem.assema(gr,x,1,1);
+[p.mat.K,p.mat.M,~]=fem.assema(gr,x,1,1); %K[r]
 
